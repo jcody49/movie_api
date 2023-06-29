4a977ff648c3b40f5a94bb3681b8ff0bf6606fe6
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-06-29 15:03:06 -0600

    removes port number
    removes port number on app.listen

--- index.js
+++ index.js
@@ -310,6 +310,6 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
+app.listen(port, () => {
  console.log('Listening on Port ' + port);
 });
